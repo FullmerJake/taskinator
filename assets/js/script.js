256636adb4479d00ca373e7c453eab5400f64cbb
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-08-20 14:48:54 -0600

    Added sub title to tasks from drop down menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,22 @@ var createTaskHandler = function (){
     //stops the browser from refreashing immediately after the form is submitted. 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    //the DOM creates an object of the select dropdown menu. when you select one of them, the value becomes that. This pulls that value. 
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+  
+    //create list item
     var listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
-    listItemEl.textContent = 'This is a new task';
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement('div');
+    //give it a class name
+    taskInfoEl.className = 'task-info';
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 
 };
