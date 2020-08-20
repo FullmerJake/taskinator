c7ac3ea811e3f5bfdecc80ac17c3989fc6b4e56f
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-08-20 02:59:19 -0600

    added event method to stop form behaviour and autorefresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
 //creates a new element in the HTML in browser when the eventListener is triggered.
 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
 
 
 var createTaskHandler = function (){
+
+    //stops the browser from refreashing immediately after the form is submitted. 
+    event.preventDefault();
+
     var listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
     listItemEl.textContent = 'This is a new task';
     tasksToDoEl.appendChild(listItemEl);
+
 };
 
 // listens for a click on the button DOM element, executes the createTaskHandler function. 
-buttonEl.addEventListener('click', createTaskHandler);
+formEl.addEventListener('submit', createTaskHandler);
 
 var createTaskHandler = function (){
     var listItemEl = document.createElement('li');
