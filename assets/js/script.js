c6f36f0701cadede481c155636b7c63c9324b51e
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-08-20 15:53:52 -0600

    refactored code into 2 functions, instead of 1 long one

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,37 +3,47 @@
 var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
-
-
-var createTaskHandler = function (){
+var taskFormHandler = function (){
 
     //stops the browser from refreashing immediately after the form is submitted. 
     event.preventDefault();
-
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     //the DOM creates an object of the select dropdown menu. when you select one of them, the value becomes that. This pulls that value. 
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    
-  
+
+    //package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    //send it as an argument to createTaskEl
+    //we've created an object and passed it to another function
+    createTaskEl(taskDataObj);
+
+};
+
+var createTaskEl = function(taskDataObj){
     //create list item
     var listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
+  
     //create div to hold task info and add to list item
     var taskInfoEl = document.createElement('div');
     //give it a class name
     taskInfoEl.className = 'task-info';
     //add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    
     listItemEl.appendChild(taskInfoEl);
     //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+}
 
-};
+// listens for a click on the button DOM element, executes the taskFormHandler function. 
+formEl.addEventListener('submit', taskFormHandler);
 
-// listens for a click on the button DOM element, executes the createTaskHandler function. 
-formEl.addEventListener('submit', createTaskHandler);
-
-var createTaskHandler = function (){
+var taskFormHandler = function (){
     var listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
     listItemEl.textContent = 'This is a new task';
